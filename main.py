0c577c6b238f6a948eb50bb28aa9c3fd499a2bc1
LeeviMakinen <leevi.kalle.makinen@gmail.com>
2024-11-02 16:29:26 +0200

    Kaikki uusiks käytännössä :D

--- main.py
+++
@@ -1,7 +0,0 @@
-import tkinter as tk
-from dual_adc_analyzer import DualADCSignalAnalyzer
-
-if __name__ == "__main__":
-    root = tk.Tk()
-    app = DualADCSignalAnalyzer(root)
-    root.mainloop()
--- utils.py
+++
@@ -1,6 +0,0 @@
-# Constants
-CHUNK_SIZE = 50000
-SAMPLE_RATE = \
-    50000
-DISPLAY_TIME = 10  # Display 10 seconds of data
-ANIMATION_SPEED = 10
